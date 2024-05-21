fc6db3851bc3b8cb3c74a97ea1b4404bca1f3048
Uttam M <53312923+uttammm@users.noreply.github.com>
2024-05-22 03:34:36 +0530

    started working on projects

---
+++ src/components/projects.js
@@ -0,0 +1,11 @@
+//create a yaml doc to store projects 
+//import yaml file here
+
+export function Projects(){
+    //return an array of project items
+}
+
+
+function ProjectItem(){
+    //add project item definition
+}
